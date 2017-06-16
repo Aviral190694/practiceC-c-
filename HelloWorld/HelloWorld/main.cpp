4afc849cf3727fa6ecf548390119b49061267a33
Aviral Aggarwal <aviral190694@gmail.com>
2017-06-16 08:28:36 +0530

    First Commit with Hello World.

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Aviral Aggarwal on 16/06/17.
+//  Copyright © 2017 Aviral Aggarwal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
