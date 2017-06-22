2e64fc35fd492c5c1f9ce8917bd951b87df039ef
Aviral Aggarwal <aviral190694@gmail.com>
2017-06-22 18:14:44 +0530

    Project setup and Cleaning Mac

---
+++ Hello.cpp
@@ -0,0 +1,19 @@
+// #include <iostream>
+// #include <stdio.h>
+
+// using namespace std;
+
+// int main() {
+// 	// your code goes here
+// 	// ios_base::sync_with_stdio(false);
+// 	// std::cin.tie(NULL);
+// 	std::cout<<"Hello World";
+// 	return 0;
+// }
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ LearnC++/Linked list/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Linked list
+//
+//  Created by Aviral Aggarwal on 16/06/17.
+//  Copyright © 2017 Aviral Aggarwal. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World I am a Linked list!\n";
+    return 0;
+}
+
