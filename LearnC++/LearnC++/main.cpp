391b4e3126c5dcb30f8f0494d3c274aa581c8b3a
Aviral Aggarwal <aviral190694@gmail.com>
2017-06-25 19:41:46 +0530

    Learning about modifier Types, storeage classes.

--- LearnC++/LearnC++/main.cpp
+++ LearnC++/LearnC++/main.cpp
@@ -7,9 +7,26 @@
 //
 
 #include <iostream>
+using namespace std;
+#define LENGTH 10
+#define NEWLINE '\n'
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+//    cout << "Hello, World!\n";
+    const int area = 100;
+    //Checking for Carraige Return and all
+//    cout << "Hello\tWorld\n\n";
+    cout<<LENGTH<<NEWLINE<<area;
+    
+    //Testing Signed and Unsigned
+    
+    short int i;
+    short unsigned int j;
+    j=50000;
+    i=j;
+    cout<<i<<" "<<j;
+
+    
     return 0;
 }
