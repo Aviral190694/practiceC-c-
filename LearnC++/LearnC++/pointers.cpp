82121ceef2fef587ef640edd69d08e0ee36c81d8
Aviral Aggarwal <aviral190694@gmail.com>
2017-07-05 23:35:43 +0530

    learning Classes, pointers and friend functions.

--- LearnC++/LearnC++/pointers.cpp
+++ LearnC++/LearnC++/pointers.cpp
@@ -2,9 +2,105 @@
 
 using namespace std;
 
+class two;
+
+class test
+{
+    private:
+        int width;
+        int *ptr;
+    public :
+        void setWidth(int length);
+        int getWidth();
+        int getPointer();
+        void setPointer(int l);
+    friend void display(test onj);
+    void testFriend(two &obj);
+        test();
+        test(const test &obj);
+};
+
+class two{
+    int a,b;
+public:
+    friend void test::testFriend(two &obj);
+    two() : a(1), b(2)
+    {
+        
+    }
+};
+
+void test::testFriend(two &obj)
+{
+    
+}
+    test::test(const test &obj)
+    {
+        cout<<"copy constructor";
+        width = obj.width;
+        ptr = new int();
+        *ptr = *obj.ptr;
+    }
+
+test::test()
+{
+    cout<<"This is a test constructor";
+    ptr = new int;
+    *ptr = 10;
+}
+
+void test::setWidth(int width)
+{
+    this->width = width;
+}
+
+void test::setPointer(int l)
+{
+    *ptr = l;
+}
+
+
+int test::getWidth()
+{
+    return width;
+}
+int test::getPointer()
+{
+    return *ptr;
+}
+
+void display(test obj)
+{
+    cout<<endl<<obj.width;
+}
+
 void getSeconds(unsigned long *par);
+double getAverage(int *arr,int size);
+int * getRandom( ) {
+    static int  r[10];
+    
+    // set the seed
+    srand( (unsigned)time( NULL ) );
+    
+    for (int i = 0; i < 10; ++i) {
+        r[i] = rand();
+        cout << r[i] << endl;
+    }
+    
+    return r;
+}
 
 int main () {
+    
+    test newTest;
+    newTest.setWidth(5);
+    cout<<newTest.getWidth()<<" "<<newTest.getPointer();
+    
+    test second(newTest);
+    newTest.setPointer(20);
+    cout<<endl<<second.getWidth()<<" "<<second.getPointer()<<endl;
+    display(newTest);
+    
     int var1;
     char var2[10];
     cout << "Address of var1 variable: ";
@@ -27,7 +123,7 @@ int main () {
     *(arr + 2) = 500;
     
     ptr = arr;
-//    cout<<endl<<" "<<arr<<" "<<arr[0]<<" "<<&arr;
+    //    cout<<endl<<" "<<arr<<" "<<arr[0]<<" "<<&arr;
     for(int i=0;i<3;i++)
     {
         cout<<endl<<ptr<<" "<<*ptr;
@@ -55,8 +151,47 @@ int main () {
     cout <<endl<< "Value of var :" << val << endl;
     cout << "Value available at *singlePtr :" << *singlePtr << endl;
     cout << "Value available at **doublePtr :" << **doublePtr << endl;
-
+    
+    unsigned long sec;
+    getSeconds(&sec);
+    cout<<endl<<"Nummer of seconds :"<<sec;
+    
+    
+    int balance[5] = {1000, 2, 3, 17, 50};
+    double avg;
+    
+    // pass pointer to the array as an argument.
+    avg = getAverage( balance, 5 ) ;
+    
+    // output the returned value
+    cout <<endl<< "Average value is: " << avg;
+    
+    int *pp;
+    
+    pp = getRandom();
+    
+    for ( int i = 0; i < 10; i++ ) {
+        cout << "*(pp + " << i << ") : ";
+        cout << *(pp + i) <<" "<<pp[i]<< endl;
+    }
     
     cout<<endl;
+    
     return 0;
+}
+
+void getSeconds(unsigned long *par)
+{
+    *par = time(NULL);
+    return;
+}
+
+double getAverage(int *arr,int size)
+{
+    double avg = 0.0;
+    for (int i=0; i<size; i++) {
+        avg = avg + *(arr + i);
+        //or avg += arr[i];
+    }
+    return avg/size;
 }
